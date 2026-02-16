83de93a3201c78f5c85ac98d6f13510cb9c174a8
Silas Hsu <hsu.silas@wustl.edu>
2026-02-15 19:44:08 -0600

    Removed TS and shared package structure

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import dotenv from "dotenv";
 
 dotenv.config(); // Read the .env file in the current working directory, and load values into process.env.
@@ -6,10 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get("/hello", (req: Request, res: Response) => {
+app.get("/hello", (req, res) => {
     res.send("Hello, World");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}.  CTRL+C to stop.`);
 });
