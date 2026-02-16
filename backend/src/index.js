432f7673a64965d133630427584281cf77941938
Silas Hsu <hsu.silas@wustl.edu>
2026-02-16 14:33:34 -0600

    Added symlinks to shared folder

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { SHARED_TEST } from "./shared/example.js";
 
 dotenv.config(); // Read the .env file in the current working directory, and load values into process.env.
 const PORT = process.env.PORT || 3000;
@@ -7,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get("/hello", (req, res) => {
-    res.send("Hello, World");
+    res.send("Hello, World " + SHARED_TEST);
 });
 
 app.listen(PORT, () => {
